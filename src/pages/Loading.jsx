f12ad07d76d87dee5f3a046ec0878754baf9d54c
Joey Hetfield <joeydefenix@hotmail.com>
2022-11-03 19:23:05 -0300

    Requisito 2 - Parte de Carregando feita

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Loading extends Component {
+  render() {
+    return (
+      <div>
+        Carregando...
+      </div>
+    );
+  }
+}
+
+export default Loading;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,12 @@
 import { Component } from 'react';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends Component {
   state = {
     isDisabled: true,
     user: '',
+    isLoad: false,
   };
 
   handleChangeUser = (event) => {
@@ -17,29 +20,40 @@ class Login extends Component {
     });
   };
 
+  handleCLick = () => {
+    const { user } = this.state;
+    this.setState({
+      isLoad: true,
+    });
+    createUser({ name: user });
+  };
+
   render() {
-    const { isDisabled, user } = this.state;
+    const { isDisabled, user, isLoad } = this.state;
     return (
       <div data-testid="page-login">
-        <form>
-          <label htmlFor="inputName">
-            Usuario:
-            <input
-              type="text"
-              value={ user }
-              name="inputName"
-              onChange={ this.handleChangeUser }
-              data-testid="login-name-input"
-            />
-          </label>
-          <button
-            disabled={ isDisabled }
-            type="button"
-            data-testid="login-submit-button"
-          >
-            Entrar
-          </button>
-        </form>
+        { isLoad ? <Loading /> : (
+          <form>
+            <label htmlFor="inputName">
+              Usuario:
+              <input
+                type="text"
+                value={ user }
+                name="inputName"
+                onChange={ this.handleChangeUser }
+                data-testid="login-name-input"
+              />
+            </label>
+            <button
+              disabled={ isDisabled }
+              type="button"
+              data-testid="login-submit-button"
+              onClick={ this.handleCLick }
+            >
+              Entrar
+            </button>
+          </form>
+        )}
       </div>
     );
   }

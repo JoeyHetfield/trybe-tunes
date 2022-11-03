1833ce24c5b27cbd731b2da5dbbf6c69bf28608e
Joey Hetfield <joeydefenix@hotmail.com>
2022-11-03 15:44:51 -0300

    Requisito 1!!

---
+++ src/pages/Album.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Album extends Component {
+  render() {
+    return (
+      <div data-testid="page-album">
+        a
+      </div>
+    );
+  }
+}
+
+export default Album;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        aaa
+      </div>
+    );
+  }
+}
+
+export default Login;

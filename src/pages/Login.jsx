10962820db556774bc4cc07039b1f6a4446b026f
Joey Hetfield <joeydefenix@hotmail.com>
2022-11-03 17:52:33 -0300

    Requisito 2 Botão habilitar

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,45 @@
 import { Component } from 'react';
 
 class Login extends Component {
+  state = {
+    isDisabled: true,
+    user: '',
+  };
+
+  handleChangeUser = (event) => {
+    this.setState({
+      user: event.target.value,
+    }, () => {
+      const { user } = this.state;
+      const lengthUser = user.length;
+      const maxLength = 3;
+      this.setState({ isDisabled: lengthUser < maxLength });
+    });
+  };
+
   render() {
+    const { isDisabled, user } = this.state;
     return (
       <div data-testid="page-login">
-        aaa
+        <form>
+          <label htmlFor="inputName">
+            Usuario:
+            <input
+              type="text"
+              value={ user }
+              name="inputName"
+              onChange={ this.handleChangeUser }
+              data-testid="login-name-input"
+            />
+          </label>
+          <button
+            disabled={ isDisabled }
+            type="button"
+            data-testid="login-submit-button"
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }

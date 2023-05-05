9661186431aa71f0ad776a308469b3d7eedc8586
Joey Hetfield <joeydefenix@hotmail.com>
2023-05-04 21:03:19 -0300

    changes

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ class NotFound extends Component {
   render() {
     return (
       <div data-testid="page-not-found">
-        Página não encontrada
+        Página não encontrada!
       </div>
     );
   }
